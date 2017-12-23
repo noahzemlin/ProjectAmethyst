e27abbdfa3f446505fa010e7fa228e9ba6063f16
Noah Zemlin <noah.zemlin@ou.edu>
2017-12-23 03:47:03 -0600

    Basic data retrieval and random selection; Basic UI

---
+++ ProjectAmethyst/AmethystCore.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAmethyst
+{
+    public static class AmethystCore
+    {
+        public static string currentChampion = "";
+
+        public static Random rng = new Random();
+
+        private static List<string> champions = new List<string>();
+
+        public static void LoadChampions()
+        {
+
+            string jsonRaw;
+
+            using (WebClient wc = new WebClient())
+            {
+                jsonRaw = wc.DownloadString("http://ddragon.leagueoflegends.com/cdn/7.24.2/data/en_US/champion.json");
+            }
+
+            dynamic json = JsonConvert.DeserializeObject(jsonRaw);
+
+            dynamic jsondata = json.data;
+
+            foreach(dynamic champ in jsondata)
+            {
+                champions.Add(champ.Name);
+            }
+        }
+
+        public static void GetChampion()
+        {
+            currentChampion = champions[rng.Next(champions.Count)];
+        }
+    }
+}
--- ProjectAmethyst/MainWindow.xaml.cs
+++ ProjectAmethyst/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,69 @@ using System.Windows.Shapes;
 
 namespace ProjectAmethyst
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private bool invisible = true;
+
         public MainWindow()
         {
+
+            AmethystCore.LoadChampions();
+
             InitializeComponent();
+
+
         }
+
+
+        private void genChamp_Click(object sender, RoutedEventArgs e)
+        {
+            AmethystCore.GetChampion();
+
+            ((Label)this.FindName("champName")).Content = AmethystCore.currentChampion;
+
+            BitmapImage logo = new BitmapImage();
+            logo.BeginInit();
+            logo.UriSource = new Uri("http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + AmethystCore.currentChampion + ".png");
+            logo.EndInit();
+
+            ((Image)this.FindName("champImage")).Source = logo;
+
+            if (invisible)
+            {
+                invisible = false;
+                ((Grid)this.FindName("champGrid")).Visibility = Visibility.Visible;
+            }
+        }
+
+        private void buttonLAL_Click(object sender, RoutedEventArgs e)
+        {
+            openURL("https://lolalytics.com/champion/" + AmethystCore.currentChampion + "/");
+        }
+
+        private void buttonOPGG_Click(object sender, RoutedEventArgs e)
+        {
+            openURL("https://op.gg/champion/" + AmethystCore.currentChampion + "/");
+        }
+
+        private void buttonCGG_Click(object sender, RoutedEventArgs e)
+        {
+            openURL("http://champion.gg/champion/" + AmethystCore.currentChampion + "/");
+        }
+
+        private void openURL(String url)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(url);
+            }
+            catch { }
+        }
+
     }
 }
