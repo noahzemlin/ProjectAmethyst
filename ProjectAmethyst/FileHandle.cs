f7c98297c21939d3d987666e19bf28c78611cea5
Justin Wilford <JTKewl@gmail.com>
2017-12-24 03:49:28 -0600

    Added a file handler class and basic setting object

---
+++ ProjectAmethyst/FileHandle.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ProjectAmethyst
+{
+    public static class FileHandle
+    {
+        private static String _LOCATION = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\ProjectAmethyst\\";
+        private static String _FILENAME = "settings.cfg";
+
+        private static LinkedList<Setting> settings;
+
+        public static void addSetting()
+        {
+
+        }
+
+        public static void readSettings()
+        {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(_LOCATION + _FILENAME);
+
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                if (!System.IO.Directory.Exists(_LOCATION))
+                {
+                    Console.WriteLine("Directory did not exist! creating now...");
+                    System.IO.Directory.CreateDirectory(_LOCATION);
+                    Console.WriteLine("Created Directory at: " + _LOCATION);
+                }
+                if (!System.IO.File.Exists(_LOCATION + _FILENAME))
+                {
+                    Console.WriteLine("File did not exist! creating now...");
+                    System.IO.File.Create(_LOCATION + _FILENAME);
+                    Console.WriteLine("Created File at: " + _LOCATION + _FILENAME);
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        public static void writeSettings()
+        {
+            try
+            {
+
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter(_LOCATION + _FILENAME);
+
+                //Write a line of text
+                sw.WriteLine("Hello World!!");
+
+                //Write a second line of text
+                sw.WriteLine("From the StreamWriter class");
+
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                if (!System.IO.Directory.Exists(_LOCATION))
+                {
+                    Console.WriteLine("Directory did not exist! creating now...");
+                    System.IO.Directory.CreateDirectory(_LOCATION);
+                    Console.WriteLine("Created Directory at: " + _LOCATION);
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Completed Successfully. Location: " + _LOCATION + _FILENAME);
+            }
+        }
+    }
+}
---
+++ ProjectAmethyst/Setting.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAmethyst
+{
+    public class Setting
+    {
+        public const int INFO = 0, CHECK = 1;
+
+        private String label = "";
+        private int type = -1;
+        private int numValue = -1;
+        private String strValue = "";
+        private bool hidden = true;
+
+        public Setting(String label, int type, int numValue, bool isHidden)
+        {
+            this.label = label;
+            this.type = type;
+            this.numValue = numValue;
+            this.hidden = isHidden;
+        }
+        public Setting(String label, int type, String strValue, bool isHidden)
+        {
+            this.label = label;
+            this.type = type;
+            this.strValue = strValue;
+            this.hidden = isHidden;
+        }
+
+        public override string ToString()
+        {
+            String realValue = "";
+            if (this.type == INFO)
+                realValue = strValue;
+            else if (this.type == CHECK)
+                realValue += numValue;
+            return (this.label + " :" + 
+                    "\n\ttype :" + this.type + 
+                    "\n\tvalue : " + realValue);
+        }
+    }
+}
